08182525e46c70eaf66feebea84780f0e2826ce7
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-03-30 20:52:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif phrase.lowcase
+    return "HUH?! SPEAK UP, SONNY!"  
 
+
+
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
